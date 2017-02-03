e5a78b220cc65477ec641e8aeacd324f8b91fed9
mark757 <macunan90@gmail.com>
2017-02-03 14:50:12 -0500

    week9proj24 complete

--- week9-week9_24.MultipleEntryDictionary/src/dictionary/MultipleEntryDictionary.java
+++ week9-week9_24.MultipleEntryDictionary/src/dictionary/MultipleEntryDictionary.java
@@ -4,6 +4,8 @@ import java.util.Set;
 
 public interface MultipleEntryDictionary {
     void add(String word, String entry);
+
     Set<String> translate(String word);
+
     void remove(String word);
 }
